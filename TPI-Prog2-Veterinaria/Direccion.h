b9a85d1aebb4b56d8376e47bbce5e4f15cf9fac6
nahuelfranco7 <nahuelfranco7@gmail.com>
2025-11-01 20:40:08 -0300

    Direccion.h y .cpp

---
+++ TPI-Prog2-Veterinaria/Direccion.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <cstring>
+
+class Direccion {
+#pragma once
+#include <cstring> // para strncpy
+
+class Direccion {
+private:
+    char _pais[40];
+    char _provincia[30];
+    char _ciudad[50];
+    char _calle[50];
+    int _altura;
+    int _codPostal;
+
+public:
+    // Constructor por defecto
+    Direccion();
+
+    // Getters
+    const char* getPais();
+    const char* getProvincia();
+    const char* getCiudad();
+    const char* getCalle();
+    int getAltura();
+    int getCodPostal();
+
+    // Setters
+    void setPais(const char* pais);
+    void setProvincia(const char* provincia);
+    void setCiudad(const char* ciudad);
+    void setCalle(const char* calle);
+    void setAltura(int altura);
+    void setCodPostal(int codPostal);
+
+};
