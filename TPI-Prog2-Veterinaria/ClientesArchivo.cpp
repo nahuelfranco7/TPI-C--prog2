7efd3519b9894fbf616dde4a27b556e9682d0552
nahuelfranco7 <nahuelfranco7@gmail.com>
2025-11-12 19:21:05 -0300

    Cargar clientes solucionada

--- TPI-Prog2-Veterinaria/ClientesArchivo.cpp
+++ TPI-Prog2-Veterinaria/ClientesArchivo.cpp
@@ -56,17 +56,54 @@ int ClientesArchivo::generarNuevoID(){
 }
 
 //cargar cadena------------------------------------------
+
 void ClientesArchivo::cargarCadena(char *palabra, int tam){
-    int i = 0;
-    fflush(stdin);
-    for (i = 0; i < tam; i++){
-        palabra[i] = cin.get();
-        if (palabra[i] == '\n') break;
+    int i;
+    bool valido=false;
+
+    while(!valido){
+        fflush(stdin);
+        i=0;
+
+        cout<<"Ingrese (máximo "<<tam-1<<" caracteres): ";
+
+        for(i=0;i<tam-1;i++){
+            palabra[i]=cin.get();
+            if(palabra[i]=='\n')break;
+        }
+        palabra[i]='\0';
+        fflush(stdin);
+
+        //Si el usuario paso el límite sin presionar enter
+        if(i==tam-1 && palabra[i-1]!='\n'){
+            cout<<"Error: no puede superar los "<<tam-1<<" caracteres.\n";
+
+            //Limpiamos el resto de la cadena y descartamos lo que sobra en el buffer(almacenamiento)
+            char basura;
+            while((basura=cin.get())!='\n');
+
+            // Borrar la cadena ingresada para que no quede basura
+            palabra[0]='\0';
+        }
+        else {
+            valido=true;
+        }
     }
-    palabra[i] = '\0';
-    fflush(stdin);
 }
+
 /*----------------------------------------------------*/
+bool ClientesArchivo::cargarClientes(const Clientes &reg){
+    FILE *p = fopen(_nombreArchivo, "ab");
+    if (p == nullptr){
+        cout << "ERROR AL ABRIR EL ARCHIVO DE CLIENTES." << endl;
+        return false;
+    }
+
+    bool escribio = fwrite(&reg, sizeof(Clientes), 1, p);
+    fclose(p);
+
+    return escribio;
+}
 
 bool ClientesArchivo::cargarClientes(){
 /*
@@ -82,7 +119,7 @@ bool ClientesArchivo::cargarClientes(){
 */
     Clientes reg;
     char nombre[20];
-    char apellido[30];
+    char apellido[20];
     char DNI[20];
     Direccion direccion;
     char telefono[15];
@@ -94,7 +131,7 @@ bool ClientesArchivo::cargarClientes(){
     reg.setNombre(nombre);
 
     cout<<"INGRESE APELLIDO DEL CLIENTE: "<<endl;
-    cargarCadena(apellido,sizeof(apellido));
+    cargarCadena(apellido,20);
     reg.setApellido(apellido);
 
     cout<<"INGRESE DNI: "<<endl;
@@ -111,4 +148,13 @@ bool ClientesArchivo::cargarClientes(){
     cout<<"EMAIL: "<<endl;
     cargarCadena(email,sizeof(email));
     reg.setEmail(email);
+
+    reg.setEstado(true); // activo por defecto
+    if (cargarClientes(reg)){
+        cout << "CLIENTE GUARDADO EXITOSAMENTE." << endl;
+        return true;
+    } else {
+        cout << "ERROR AL GUARDAR EL CLIENTE." << endl;
+        return false;
+    }
 }
