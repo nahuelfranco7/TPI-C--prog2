a200f50246ae5ac9c687e35d85d7824052bba104
nahuelfranco7 <nahuelfranco7@gmail.com>
2025-11-14 20:43:04 -0300

    VeterinarioArchivo.h

---
+++ TPI-Prog2-Veterinaria/VeterinarioArchivo.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <cstring>
+#include "Veterinario.h"
+
+class VeterinarioArchivo {
+private:
+    char _nombreArchivo[30]="Veterinarios.dat";
+
+public:
+    const char* getNombreArchivo() const { return _nombreArchivo; }
+    void setNombreArchivo(const char* nombre) { strcpy(_nombreArchivo, nombre); }
+
+    int contarRegistros();
+    int buscarPorId(int id);
+    int generarNuevoID();
+
+    void cargarCadena(char *palabra, int tam);
+
+    bool cargarVet();
+    bool cargarVet(const Veterinario &reg);
+
+    bool leerVet(int pos, Veterinario &reg);
+    void mostrarVeterinario(int pos, const Veterinario &reg);
+
+    void listarTodos();
+
+    bool modificar(int pos);
+    bool modificarVet(int pos, const Veterinario &reg);
+
+    bool eliminar(int pos);
+};
