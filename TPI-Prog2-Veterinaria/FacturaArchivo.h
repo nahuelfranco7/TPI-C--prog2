b8b370d94181d2bb0408a47fd155aa89b0b11a17
nahuelfranco7 <nahuelfranco7@gmail.com>
2025-11-14 21:41:05 -0300

    FacturaArchivo.h nuevo

--- TPI-Prog2-Veterinaria/FacturaArchivo.h
+++ TPI-Prog2-Veterinaria/FacturaArchivo.h
@@ -1,29 +1,45 @@
 #pragma once
 #include <cstring>
 #include "Factura.h"
+#include "Fecha.h"
 
 class FacturaArchivo {
-
 private:
-    const char _nombreArchivo[30] = "Facturas.dat";
+    char _nombreArchivo[30] = "Facturas.dat";
 
 public:
+
+    //getters
     const char* getNombreArchivo();
 
+    //funciones de archivo
+    bool cargarFactura(const Factura &reg);
+    bool cargarFactura();
+
+    bool leerFactura(int pos, Factura &reg);
+
+    void mostrarFactura(int pos, const Factura &reg);
+
     int buscarPorId(int id);
+
     int contarRegistros();
     int generarNuevoID();
 
-    bool cargarFactura();
-    bool cargarFactura(const Factura &reg);
-
-    bool leerFactura(int pos,Factura &reg);
-    void mostrarFactura(int pos,const Factura &reg);
-
     void listarTodos();
 
-    bool modificar(int pos);
-    bool modificarFactura(int pos,const Factura &reg);
-
     bool eliminar(int pos);
+    bool modificar(int pos);
+
+    bool modificarFactura(int pos, const Factura &reg);
+
+    //filtros
+    void mostrarPorFecha(Fecha fecha);
+    void mostrarPorSemana(Fecha inicioSemana);
+    void mostrarPorMes(Fecha fecha);
+
+    //metodos
+    void cargarCadena(char *palabra, int tam);
+    bool fechaEsMayorIgual(Fecha a, Fecha b);
+    bool fechaEsMenorIgual(Fecha a, Fecha b);
+    Fecha sumarDias(Fecha f, int dias);
 };
